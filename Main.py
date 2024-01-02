754743f2138b53754af1c7268c6c696c345c4f54
Max Jin <maxjin6011@gmail.com>
2024-01-02 12:38:26 -0800

    linear regression
    need to figure out what this stupid number means

--- Main.py
+++ Main.py
@@ -1,2 +1,8 @@
 import pandas as p
+import numpy as numpy
+from sklearn.linear_model import LinearRegression
+
+df = p.read_csv("DataSetSunlife.csv")
+ages = df["AGES"]
+ltcare = df["LIVES IN LONG TERM CARE HOME"]
 
