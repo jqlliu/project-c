4e4d46470e84d0bedfa9547fe51554de71f22899
Nathan Lu <155280155+NathanL15@users.noreply.github.com>
2024-01-05 23:36:49 -0500

    asda

--- Main.py
+++ Main.py
@@ -1,16 +1,3 @@
-import pandas as p
-import numpy as numpy
-from sklearn.linear_model import LinearRegression
-import csv
-
-with open('testset.csv', 'w', newline='') as file:
-	writer = csv.writer(file)
-	field = ["age", "ltcare"]
-
-	for i in range(200):
-		writer.writerow(str(random.randint(40, 100)), str(random.randint(0, 1)))
-
-df = p.read_csv("DataSetSunlife.csv")
-
-reg = LinearRegression().fit(ages, ltcare)
-
+import pandas as pd
+df = pd.read_csv('DataSetSunlife.csv')
+df.head()
